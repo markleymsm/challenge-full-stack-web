4f10e717c237a44959a2433980c3179ee14fee46
Markley da Silva Mendes <markley.mendes@gmail.com>
2022-08-06 21:07:18 -0300

    Falta termina alguns ajustes e dps testar

--- client/src/components/AddStudent.vue
+++ client/src/components/AddStudent.vue
@@ -1,13 +1,93 @@
 <template>
-
+  <div class="submit-form mt-3 mx-auto">
+    <p class="headline">Cadastro de alunos</p>
+    <div v-if="!submitted">
+      <v-form ref="form" lazy-validation>
+        <v-text-field
+            v-model="student.name"
+            :rules="[(v) => !!v || 'Nome é obrigatorio']"
+            label="Title"
+            required
+        ></v-text-field>
+        <v-text-field
+            v-model="student.academic_record"
+            :rules="[(v) => !!v || 'Registro acadêmico é obrigatorio']"
+            label="Description"
+            required
+        ></v-text-field>
+        <v-text-field
+            v-model="student.email"
+            :rules="[(v) => !!v || 'E-mail é obrigatorio']"
+            label="Description"
+            required
+        ></v-text-field>
+        <v-text-field
+            v-model="student.cpf"
+            :rules="[(v) => !!v || 'CPF é obrigatorio']"
+            label="Description"
+            required
+        ></v-text-field>
+      </v-form>
+      <v-btn color="primary" class="mt-3" @click="saveStudent">Salvar</v-btn>
+    </div>
+    <div v-else>
+      <v-card class="mx-auto">
+        <v-card-title>
+          Salvo com sucesso!
+        </v-card-title>
+        <v-card-subtitle>
+          Clique no botão para adicionar novo aluno.
+        </v-card-subtitle>
+        <v-card-actions>
+          <v-btn color="success" @click="newStudent">Adicionar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </div>
+  </div>
 </template>
-
 <script>
+import StudentDataService from "../services/StudentDataService";
 export default {
-  name: "AddStudent"
-}
+  name: "add-student",
+  data() {
+    return {
+      student: {
+        id: null,
+        name: "",
+        academic_record: "",
+        email: "",
+        cpf: "",
+      },
+      submitted: false,
+    };
+  },
+  methods: {
+    saveStudent() {
+      let data = {
+        name: this.student.name,
+        academic_record: this.student.academic_record,
+        email: this.student.email,
+        cpf: this.student.cpf,
+      };
+      StudentDataService.create(data)
+          .then((response) => {
+            this.student.id = response.data.id;
+            console.log(response.data);
+            this.submitted = true;
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+    },
+    newStudent() {
+      this.submitted = false;
+      this.student = {};
+    },
+  },
+};
 </script>
-
-<style scoped>
-
+<style>
+.submit-form {
+  max-width: 300px;
+}
 </style>
--- client/src/components/StudentsList.vue
+++ client/src/components/StudentsList.vue
@@ -1,13 +1,115 @@
 <template>
-
+  <v-row align="center" class="list px-3 mx-auto">
+    <v-col cols="12" md="8">
+      <v-text-field v-model="title" label="Pesquisar pelo nome"></v-text-field>
+    </v-col>
+    <v-col cols="12" md="4">
+      <v-btn small @click="searchTitle">
+        Pesquisar
+      </v-btn>
+    </v-col>
+    <v-col cols="12" sm="12">
+      <v-card class="mx-auto" tile>
+        <v-card-title>Consulta de alunos</v-card-title>
+        <v-data-table
+            :headers="headers"
+            :items="students"
+            disable-pagination
+            :hide-default-footer="true"
+        >
+          <template v-slot:[`item.actions`]="{ item }">
+            <v-icon small class="mr-2" @click="editStudent(item.id)">mdi-pencil</v-icon>
+            <v-icon small @click="deleteStudent(item.id)">mdi-delete</v-icon>
+          </template>
+        </v-data-table>
+        <v-card-actions v-if="students.length > 0">
+          <v-btn small color="error" @click="removeAllStudents">
+            Remover todos
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
-
 <script>
+import StudentDataService from "../services/StudentDataService";
 export default {
-  name: "StudentList"
-}
+  name: "students-list",
+  data() {
+    return {
+      students: [],
+      title: "",
+      headers: [
+        { text: "Registro academico", align: "start", sortable: false, value: "academic_record" },
+        { text: "Nome", value: "name", sortable: false },
+        { text: "CPF", value: "cpf", sortable: false },
+        { text: "Ações", value: "actions", sortable: false },
+      ],
+    };
+  },
+  methods: {
+    retrieveStudents() {
+      StudentDataService.getAll()
+          .then((response) => {
+            this.students = response.data.map(this.getDisplayStudent);
+            console.log(response.data);
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+    },
+    refreshList() {
+      this.retrieveStudents();
+    },
+    removeAllStudents() {
+      StudentDataService.deleteAll()
+          .then((response) => {
+            console.log(response.data);
+            this.refreshList();
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+    },
+    searchTitle() {
+      StudentDataService.findByName(this.title)
+          .then((response) => {
+            this.students = response.data.map(this.getDisplayStudent);
+            console.log(response.data);
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+    },
+    editStudent(id) {
+      this.$router.push({ name: "student-details", params: { id: id } });
+    },
+    deleteStudent(id) {
+      StudentDataService.delete(id)
+          .then(() => {
+            this.refreshList();
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+    },
+    getDisplayStudent(student) {
+      return {
+        id: student.id,
+        name: student.name.length > 30 ? student.name.substr(0, 30) + "..." : student.name,
+        academic_record: student.academic_record.length > 30 ? student.academic_record.substr(0, 30) + "..." : student.academic_record,
+        email: student.email.length > 30 ? student.email.substr(0, 30) + "..." : student.email,
+        cpf: student.cpf.length > 11 ? student.cpf.substr(0, 11) + "..." : student.cpf,
+      };
+    },
+  },
+  mounted() {
+    this.retrieveStudents();
+  },
+};
 </script>
-
-<style scoped>
-
+<style>
+.list {
+  max-width: 750px;
+}
 </style>
